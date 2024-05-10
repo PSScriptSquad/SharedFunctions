3e194108740b16cae9d35b5b9b7e0a624eb662fe
Rtw915 <51250006+Rtw915@users.noreply.github.com>
2024-05-09 23:02:39 -0400

    Update Get-ProcessorUtilization.ps1

--- Get-ProcessorUtilization.ps1
+++ Get-ProcessorUtilization.ps1
@@ -11,38 +11,30 @@ function Get-ProcessorUtilization {
             Name: Get-ProcessorUtilization
             Author: Ryan Whitlock
             Date: 05.09.2024
-            Version: 1.0
-            Changes: Initial release
+            Version: 2.0
+            Changes: Updated logic and removed PRTG specific properties 
     #>
     [alias("Get-TaskManagerCPU")]
 
-    # Retrieves CPU utilization counter samples
-    $counterSamples = Get-Counter '\Processor Information(*)\% Processor Utility' | Select-Object -ExpandProperty CounterSamples
-    
-    Foreach ($counter in $counterSamples) {
-        # Skip if the instance name is '_total' or contains ',_total'
-        if ($counter.InstanceName -eq '_total' -or $counter.InstanceName -like '*,_total') {
-            continue
+    # Retrieves CPU utilization counter samples with a sample size of 20
+    $counterSamples = Get-Counter '\Processor Information(*)\% Processor Utility' -SampleInterval 1 -MaxSamples 20 | 
+        Select-Object -ExpandProperty CounterSamples |
+        Group-Object -Property InstanceName | ForEach-Object {
+
+            # Calculate average and maximum CPU usage for each core
+            $Stats = $_.Group | Measure-Object -Property CookedValue -Average -Maximum
+
+            # Find the latest timestamp
+            $Time = $_.Group | Measure-Object -Property Timestamp -Maximum
+
+            # Create a custom object with calculated statistics
+            [PSCustomObject]@{
+                'EndTime' = $Time.Maximum
+                'Name' = $_.Name
+                'Average' = [math]::Round(($Stats).Average, 2)
+                'Maximum' = [math]::Round(($Stats).Maximum, 2)
+            }
         }
-        
-        # Extracting processor name and core number from InstanceName
-        $name = "CPU:$($counter.InstanceName.split(',')[0]) Core:$($counter.InstanceName.split(',')[1])"
-        
-        [PSCustomObject]@{
-            Name = $name
-            Value = [math]::Round($counter.CookedValue, 2)
-            Unit = 'Percent'
-            Mode = 'Absolute'
-            Float = 1
-        }
-    }
-    
-    # Total CPU utilization
-    [PSCustomObject]@{
-        Name = 'Total'
-        Value = [math]::Round(($counterSamples | Where-Object { $_.InstanceName -eq '_Total' }).CookedValue, 2)
-        Unit = 'Percent'
-        Mode = 'Absolute'
-        Float = 1
-    }
+
+    return $counterSamples
 }
