a10b0d282073a27572cb1f57d0ec497a3f3403ee
Rtw915 <51250006+Rtw915@users.noreply.github.com>
2024-08-08 13:04:53 -0400

    Create Remove-Computer.ps1

---
+++ ActiveDirectory/Remove-Computer.ps1
@@ -0,0 +1,75 @@
+function Remove-Computer {
+    <#
+    .SYNOPSIS
+        Removes a computer from Active Directory.
+
+    .DESCRIPTION
+        This function removes a specified computer from Active Directory. It validates the existence of the computer,
+        and verifies domain connectivity before attempting to remove the computer.
+        After removal, it verifies that the computer has been successfully removed.
+
+    .PARAMETER ComputerName
+        The name of the computer to be removed from Active Directory.
+
+    .EXAMPLE
+        Remove-Computer -ComputerName "Computer123"
+        This example removes the computer named "Computer123" from Active Directory.
+
+    .NOTES
+        Name: Remove-Computer
+        Author: Ryan Whitlock
+        Date: 08.05.2024
+        Version: 1.0
+        Changes: Initial release 
+
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateScript({
+            try {
+                $Identity = Get-ADComputer -Identity $_ -ErrorAction Stop
+                if ($Identity) {
+                    $true
+                } else {
+                    throw "A computer with the name '$_' does not exist in Active Directory."
+                }
+            } catch [Microsoft.ActiveDirectory.Management.ADServerDownException] {
+                throw "No domain connectivity. Please check your network and domain settings."
+            } catch {
+                throw "A computer with the name '$_' does not exist in Active Directory."
+            }              
+        })]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName
+    )
+    
+    process {
+        # Check if ShouldProcess is allowed (used for WhatIf and Confirm)
+        if ($PSCmdlet.ShouldProcess($ComputerName, "Remove computer from Active Directory")) {
+            try {
+                # Remove the computer from Active Directory
+                Remove-ADComputer -Identity $ComputerName -Confirm:$false -ErrorAction Stop
+                Write-Output "Computer '$ComputerName' has been successfully removed from Active Directory." -ForegroundColor Green
+            }
+            catch {
+                throw "Failed to remove computer '$ComputerName' from Active Directory."
+            }
+
+            # Verify that the computer has been removed
+            try {
+                $computer = Get-ADComputer -Filter { Name -eq $ComputerName } -ErrorAction SilentlyContinue
+                if ($null -ne $computer) {
+                    throw "Computer '$ComputerName' still exists in Active Directory. Removal might have failed."
+                } else {
+                    Write-Output "Verified that computer '$ComputerName' has been removed from Active Directory." -ForegroundColor Green
+                }
+            }
+            catch {
+                throw "Error occurred while verifying the removal of computer '$ComputerName'."
+            }
+        } else {
+            Write-Output "Operation for removing computer '$ComputerName' was skipped due to WhatIf or Confirm preference." -ForegroundColor Yellow
+        }
+    }
+}
